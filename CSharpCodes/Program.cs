7e2a62ff89c0b9d564899ac94df9700eae1c37c9
Henrique <henrique@wopus.com.br>
2023-08-05 18:07:20 -0300

    Adicionar arquivos de projeto.

---
+++ CSharpCodes/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string teste;
+            /*
+             É possível declarar uma variável
+             sem valor, e atribuir o valor depois.
+            */
+            teste = "testando";
+
+            string name = "Henrique"; //string
+            char letra = 'h'; //char
+            int number = 10; //int
+            double number2 = 8.5; //double
+            bool condition = true; //bool
+
+            Console.WriteLine(name);
+            Console.WriteLine(letra);
+            Console.WriteLine(number);
+            Console.WriteLine(number2);
+            Console.WriteLine(condition);
+        }
+    }
+}
