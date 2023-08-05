272a7b7933d390ba6b687c01a87de05823c0b898
Henrique <henrique@wopus.com.br>
2023-08-05 18:42:25 -0300

    Programa de par ou ímpar e calculadora com 4 operações.

--- CSharpCodes/Program.cs
+++ CSharpCodes/Program.cs
@@ -1,38 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-namespace MyApplication
+
+namespace OddOrEven
 {
-    class Program
+    class ProgramOOE //Programa que identifica se o número é par ou ímpar.
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            string teste;
-            /*
-             É possível declarar uma variável
-             sem valor, e atribuir o valor depois.
-            */
-            teste = "testando";
+        
+            Console.Write("Escreva um número: ");
+        
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+        
+            Console.WriteLine((number % 2 == 0 ? "O número é par": "O número é ímpar"));
+        }
 
-            string name = "Henrique"; //string
-            char letra = 'h'; //char
-            int number = 10; //int
-            double number2 = 8.5; //double
-            bool condition = true; //bool
+    }
 
-            Console.WriteLine(name);
-            Console.WriteLine(letra);
-            Console.WriteLine(number);
-            Console.WriteLine(number2);
-            Console.WriteLine(condition);
+    class Calculator
+    { 
+        static void Calc(string[] args)
+        {
+            Console.WriteLine("Escreva o primeiro número:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escreva o segundo número:");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escolha a operação: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.Write("Operação escolhida: ");
+            Double operacao = Convert.ToDouble(Console.ReadLine());
 
-            if(name == "Henrique")
+            switch (operacao)
             {
-                Console.WriteLine("Meu nome é Henrique");
-            }
-            else
-            {
-                Console.WriteLine("Meu nome não é Henrique!");
+                case 1:
+                    Console.WriteLine(number1 + number2);
+                    break;
+                case 2:
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case 3:
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case 4:
+                    Console.WriteLine(number1 / number2);
+                    break;
+                default:
+                    Console.WriteLine("A operação não existe");
+                    break;
             }
         }
+
     }
+
+
 }
