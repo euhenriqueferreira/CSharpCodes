bcf536540c21a7adb310980e5c66d9e8a4483fa4
Henrique <henrique@wopus.com.br>
2023-08-10 21:13:20 -0300

    Classes e objetos, constructor, modificadores de acesso, get e set, herança, polimorfismo.

---
+++ CSharpCodes/Carro.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApplication
+{
+    class Carro
+    {
+        public string cor;
+        public int numPortas;
+        public string potencia;
+        private string numSerie;
+
+        public string NumSerie
+        {
+            get { return numSerie;  }
+            set { numSerie = value; }
+        }
+
+        public Carro(string corCarro, int numeroPortas, string potenciaCarro)
+        {
+            cor = corCarro;
+            potencia = potenciaCarro;
+            numPortas = numeroPortas;
+        }
+
+
+        public void acelerar()
+        {
+            Console.WriteLine("O carro está ganhando velocidade");
+        }
+
+        public void frear()
+        {
+            Console.WriteLine("O carro está diminuindo a velocidade");
+        }
+    }
+
+}
--- CSharpCodes/Program.cs
+++ CSharpCodes/Program.cs
@@ -1,60 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Security.Cryptography.X509Certificates;
 
-namespace OddOrEven
+namespace MyApplication
 {
-    class ProgramOOE //Programa que identifica se o número é par ou ímpar.
+    class Pessoa
     {
+        public string nome;
+        public int anoNascimento;
+        
+        public int idade()
+        {
+            return 2023 - anoNascimento;
+        }
+    }
+
+    class Adulto : Pessoa
+    {
+        public bool empregado = true;
+    }
+
+
+
+    class Program
+    {       
         static void Main(String[] args)
         {
-        
-            Console.Write("Escreva um número: ");
-        
-            string input = Console.ReadLine();
-            int number = Convert.ToInt32(input);
-        
-            Console.WriteLine((number % 2 == 0 ? "O número é par": "O número é ímpar"));
+            Carro Civic = new Carro("Roxo", 2, "800cv");
+
+            Console.WriteLine(Civic.cor);
+            Console.WriteLine(Civic.numPortas);
+            Console.WriteLine(Civic.potencia);
+
+            Civic.acelerar();
+
+            Carro Porsche = new Carro("Bronze", 2, "700cv");
+            //Porsche.NumSerie = "BNEMNR11";
+            Console.WriteLine(Porsche.NumSerie);
+
+
+            Adulto Henrique = new Adulto();
+
+            Henrique.anoNascimento = 2003;
+            Console.WriteLine("Idade: " + Henrique.idade());
+            Console.WriteLine("Empregado: " + Henrique.empregado);
         }
 
     }
-
-    class Calculator
-    { 
-        static void Calc(string[] args)
-        {
-            Console.WriteLine("Escreva o primeiro número:");
-            double number1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Escreva o segundo número:");
-            double number2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Escolha a operação: ");
-            Console.WriteLine("1 - Adição");
-            Console.WriteLine("2 - Subtração");
-            Console.WriteLine("3 - Multiplicação");
-            Console.WriteLine("4 - Divisão");
-            Console.Write("Operação escolhida: ");
-            Double operacao = Convert.ToDouble(Console.ReadLine());
-
-            switch (operacao)
-            {
-                case 1:
-                    Console.WriteLine(number1 + number2);
-                    break;
-                case 2:
-                    Console.WriteLine(number1 - number2);
-                    break;
-                case 3:
-                    Console.WriteLine(number1 * number2);
-                    break;
-                case 4:
-                    Console.WriteLine(number1 / number2);
-                    break;
-                default:
-                    Console.WriteLine("A operação não existe");
-                    break;
-            }
-        }
-
-    }
-
 
 }
